e50217c2b8df6d69662cddc8767f1630d6593604
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-07-07 23:04:50 +0000

    top bar

--- myplaylist/app/(site)/page.tsx
+++ myplaylist/app/(site)/page.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/Header";
+
+
 export default function Home() {
   return (
-    <div className="text-blue-500">
-      Main Content
-      </div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        Headeyyyy!
+      </Header>
+    </div>
   )
 }
  
